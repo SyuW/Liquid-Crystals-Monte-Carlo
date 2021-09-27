8beed497d83a479603e77b2d952e282300e4dbe9
SyuW <49294470+SyuW@users.noreply.github.com>
2021-09-27 15:38:03 -0400

    Start to LC interactions file

---
+++ interactions.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def compute_ellipse_ellipse():
+    return
+
+
+def compute_ellipse_wall():
+    return
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
